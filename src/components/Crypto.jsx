aa4c96a88b005601ac621f0d6a6daf7686592b58
veronicagimenez279 <81596467+veronicagimenez279@users.noreply.github.com>
2022-10-26 16:38:55 -0300

    Create Crypto.jsx

---
+++ src/components/Crypto.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+export const Crypto = ({ resultado }) => {
+
+    const img = `https://www.cryptocompare.com${resultado.IMAGEURL}`
+    if (Object.keys(resultado).length === 0) return null
+    return (
+        <div className="col s12 m8 offset-m2 l12">
+            <div className="card-panel grey lighten-5 z-depth-1">
+                <div className="row valign-wrapper">
+                    <div className="col s4 text-align center">
+                        <img src={img} alt="" className="circle responsive-img" />
+                        {resultado.FROMSYMBOL}
+                    </div>
+                    <div className="col s10 left-align">
+                        <span className="black-text s2">
+                            <blockquote className='text-bold'>
+                                Cotizacion del dia: {resultado.PRICE} {resultado.TOSYMBOL}
+                                <p></p>
+                                Precio más alto del dia: {resultado.HIGHDAY} {resultado.TOSYMBOL}
+                                <p></p>
+                                Precio más bajo del dia: {resultado.LOWDAY} {resultado.TOSYMBOL}
+                                <p></p>
+                                Variación de las últimas 24 horas: {resultado.VOLUME24HOUR} {resultado.TOSYMBOL}
+                            </blockquote>
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+// - Cotización del día
+// - Precio más alto del día
+// - Precio más bajo del día
+// - Variación última 24hs.
